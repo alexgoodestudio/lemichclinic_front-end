7af17760bfbd4aac62e411495b30008f4973a3ba
Alex Goode <alexgoodestudio@gmail.com>
2025-02-04 18:13:36 -0500

    pading fix

--- src/Components/Insurance.js
+++ src/Components/Insurance.js
@@ -70,7 +70,7 @@ function Insurance() {
                 highest level of confidentiality, we can work with you on a
                 private pay option. Prices vary by clinician and session length.
               </li>
-              <li className="mt-5 border bg-slate-100 p-5">
+              <li className="mt-5 border bg-slate-100 p-4">
               Are you Active Duty and not able to see your PCM quickly to get a referral? AD may present to the Care Connect station at the Behavioral Health Department in Building 3 at NMCP to meet with a Behavioral Health Technician to get a referral for mental health counseling. The Care Connect station is open Monday-Friday from 8:30-10:30. Once the referral is created, the service member will need to call Tricare to have the provider updated to The Lemich Clinic.
               </li>
             </ul>
--- src/WebRoutes.js
+++ src/WebRoutes.js
@@ -7,19 +7,20 @@ import Insurance from "./Components/Insurance";
 import Contact from "./Components/Contact";
 import Owners from "./Components/Owners";
 import Login from "./Components/Login";
-import ContactEditForm from "./Components/ContactEditForm"; 
+import ContactEditForm from "./Components/ContactEditForm";  
+import "./App.css";
 
 function WebRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} /> 
-      <Route path="team" element={<OurTeam />} />
-      <Route path="services" element={<Services />} />
-      <Route path="insurance" element={<Insurance />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="owners" element={<Owners />} />
-      <Route path="login" element={<Login />} />
-      <Route path="contacts/:contact_id/edit" element={<ContactEditForm />} /> 
+      <Route path="/" element={<div className="fade-in"><Dashboard /></div>} /> 
+      <Route path="team" element={<div className="fade-in"><OurTeam /></div>} />
+      <Route path="services" element={<div className="fade-in"><Services /></div>} />
+      <Route path="insurance" element={<div className="fade-in"><Insurance /></div>} />
+      <Route path="contact" element={<div className="fade-in"><Contact /></div>} />
+      <Route path="owners" element={<div className="fade-in"><Owners /></div>} />
+      <Route path="login" element={<div className="fade-in"><Login /></div>} />
+      <Route path="contacts/:contact_id/edit" element={<div className="fade-in"><ContactEditForm /></div>} /> 
     </Routes>
   );
 }
