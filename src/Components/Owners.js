12e5c94abaa80510635ea5cc6b9bdcc4bd59adaa
Alex Goode <alexgoodestudio@gmail.com>
2025-02-22 22:59:32 -0500

    react helmet installed and meta tags directed integrated into owners page

--- src/Components/Owners.js
+++ src/Components/Owners.js
@@ -2,6 +2,7 @@ import img4 from "./images/lemichs.avif";
 import { Link } from "react-router-dom";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import { Helmet } from "react-helmet";
 
 function Owners() {
   const logoRef = useRef(null); // Reference for the logo
@@ -18,6 +19,13 @@ function Owners() {
   }, []);
   return (
     <div className="row flex items-center justify-center padding-mobile-owners">
+        <Helmet>
+        <meta
+          name="description"
+          content="Meet the founders of The Lemich Clinic: Gregory and Jennifer Lemich, experts in mental health services for veterans and military personnel in Norfolk, VA."
+        />
+        <title>Meet The Owners | The Lemich Clinic | Norfolk, VA</title>
+      </Helmet>
       <div className="col-lg-6 p-lg-5">
         <div className=" w-full p-lg-5">
           <h1 className=" text-4xl text-start mb-lg-5  text-gray-800 mb-1">
