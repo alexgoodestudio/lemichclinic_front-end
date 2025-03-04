ab2436bf6e6c01a9d933acf91ae22cc69a4c76b3
Alex Goode <alexgoodestudio@gmail.com>
2025-03-03 22:45:35 -0500

    resize mobile animation text2

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css"
 import "./Components/animations/useGsapAnimations"
+import logo from "../src/Components/images/black.png"
 
 function Navbar() {
 
   return (
     <nav className="navbar main">
       <div className="container  overflow-hidden">
-        <Link className="logo-location navbar-brand  d-flex" to="/">
-          <span className="">The Lemich Clinic  </span>  <span className="thin  ms-2">|&nbsp; Norfolk, Virginia </span>
+        <Link className="logo-location navbar-brand d-flex" to="/">
+          <img className="tiny" src={logo} alt="logo"  />
+          <span className="">The Lemich Clinic  </span><span className="thin  ms-2">| Norfolk, VA</span>
         </Link>
         <ul className="navbar-nav  ms-auto d-flex flex-row gap-4 ivy nav-link-styling">
   <li className="nav-item">
