7a248841b2267dde6d6fda036049514aca72970d
Alex Goode <alexgoodestudio@gmail.com>
2025-03-07 21:05:34 -0500

    corral accent

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -86,7 +86,7 @@ function Dashboard() {
               <i className="fas fa-phone-alt text-md "></i>
               <a href="tel:+17575361233" className="phone-link mobile-bold ">
                 <span className="hideMobile">&nbsp;</span>{" "}
-                <span className="phone-button-bold">Call Now</span>
+                <span className="phone-button-bold accent">Call Now</span>
                 <span className="hideMobile smallText">&nbsp;+1(757) 536-1233 </span>
               </a>
             </div>
